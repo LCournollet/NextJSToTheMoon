c50c427e6c87155fb6b7a29d7e7ad64452e6bd97
Louis <l.cournollet@gmail.Com>
2026-03-03 11:33:18 +0100

    exo2

--- pages/mentions.tsx
+++ pages/mentions.tsx
@@ -1,7 +1,12 @@
+import Mentions from "../components/mentions/Mentions.md";
+import { ButtonLink } from "@/components/ui/ButtonLink";
+
 export default function MentionsPage() {
   return (
     <main>
       <h1>Mentions</h1>
+      <Mentions />
+      <ButtonLink href="/">Go back to home</ButtonLink>
     </main>
   );
 }
